1cfede4d2b1966c3f14d0067de5823c6ff980ec3
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-09-11 18:27:13 +0200

    chore: add path to error handler

--- error-handler.ts
+++ error-handler.ts
@@ -2,6 +2,7 @@ export default defineNitroErrorHandler((error, event) => {
   setResponseHeader(event, "Content-Type", "application/json");
 
   return send(event, JSON.stringify({
+    path: event.path,
     timestamp: new Date().toISOString(),
     message: error.message,
     status: error.statusCode || 500,
