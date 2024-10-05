69b9873e829188ed32d08b16e3916c61d47ac575
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-10-05 09:05:29 +0200

    chore: merge migrations scripts

--- migrations/0001_initialize.sql
+++ migrations/0001_initialize.sql
@@ -1,14 +1,11 @@
 CREATE TABLE IF NOT EXISTS mosaic_repositories (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  github_id INTEGER NOT NULL UNIQUE,
+  github_id VARCHAR NOT NULL UNIQUE,
   name_with_owner TEXT NOT NULL UNIQUE,
   description TEXT,
   name TEXT NOT NULL,
   url TEXT NOT NULL,
   last_updated TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
   config JSON NOT NULL DEFAULT '{}'
 );
-
-CREATE INDEX idx_mosaic_repositories_github_id ON mosaic_repositories(github_id);
-CREATE INDEX idx_mosaic_repositories_name ON mosaic_repositories(name);
-CREATE INDEX idx_mosaic_repositories_last_updated ON mosaic_repositories(last_updated);
--- migrations/0003_github_id.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE mosaic_repositories
-    ALTER COLUMN github_id SET DATA TYPE VARCHAR
-    USING github_id::VARCHAR;
