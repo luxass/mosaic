99fb231c611417069e671cce91fd9d0406cf822c
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-10-06 05:51:58 +0200

    refactor: simplify json schema endpoint handler
    Refactored the `get_json_schema` function to return a `RootSchema` directly instead of serialized JSON. Introduced a handler `json_schema_handler` to manage the HTTP response including JSON serialization in the routes.

--- crates/utils/src/json_schema.rs
+++ crates/utils/src/json_schema.rs
@@ -1,23 +1,12 @@
-use axum::response::IntoResponse;
 use schemars::gen::SchemaSettings;
+use schemars::schema::RootSchema;
+use crate::{MosaicConfig};
 
-use crate::MosaicConfig;
-
-pub async fn get_json_schema() -> impl IntoResponse {
+pub async fn get_json_schema() -> RootSchema {
   let settings = SchemaSettings::draft07().with(|s| {
     s.option_nullable = true;
     s.option_add_null_type = false;
   });
   let generator = settings.into_generator();
-  let schema = generator.into_root_schema_for::<MosaicConfig>();
-
-  // TODO: handle this error
-
-  match serde_json::to_string_pretty(&schema) {
-    Ok(json_schema) => json_schema,
-    Err(err) => {
-      tracing::error!("failed to serialize json schema: {}", err);
-      "{}".to_string()
-    }
-  }
+  generator.into_root_schema_for::<MosaicConfig>()
 }
