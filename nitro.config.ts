16f341e4bacc0e1f293d81015ba57c2cbc78676d
Lucas <lucasnrgaard@gmail.com>
2024-11-24 12:03:08 +0100

    chore: set preset to cloudflare

--- nitro.config.ts
+++ nitro.config.ts
@@ -9,6 +9,7 @@ export default defineNitroConfig({
     // eslint-disable-next-line node/prefer-global/process
     siteUrl: process.env.DEPLOY_URL ? process.env.URL : "http://localhost:3000",
   },
+  preset: "cloudflare-pages",
   compatibilityDate: "2024-09-11",
   typescript: {
     strict: true,
