61a7df0d6ff046937b4a60b6610476f3c5660b6d
Lucas <lucasnrgaard@gmail.com>
2024-11-24 12:09:23 +0100

    debug

--- api/v1/mosaic/[username]/contributions.get.ts
+++ api/v1/mosaic/[username]/contributions.get.ts
@@ -6,28 +6,36 @@ export default defineLazyEventHandler(async () => {
   const runtimeConfig = useRuntimeConfig();
 
   return defineCachedEventHandler(async (event) => {
-    const username = getRouterParam(event, "username");
+    try {
+      const username = getRouterParam(event, "username");
 
-    if (!username) {
+      if (!username) {
+        throw createError({
+          status: 400,
+          message: "missing params",
+        });
+      }
+
+      const { user } = await graphql<{
+        user: {
+          contributions: User["repositoriesContributedTo"];
+        };
+      }>(PROFILE_CONTRIBUTIONS_QUERY, {
+        headers: {
+          "Authorization": `Bearer ${runtimeConfig.github.token}`,
+          "Content-Type": "application/json",
+        },
+        login: username,
+      });
+
+      return user?.contributions?.nodes || [];
+    } catch (err) {
+      console.error(err);
       throw createError({
-        status: 400,
-        message: "missing params",
+        status: 500,
+        message: "internal server error",
       });
     }
-
-    const { user } = await graphql<{
-      user: {
-        contributions: User["repositoriesContributedTo"];
-      };
-    }>(PROFILE_CONTRIBUTIONS_QUERY, {
-      headers: {
-        "Authorization": `Bearer ${runtimeConfig.github.token}`,
-        "Content-Type": "application/json",
-      },
-      login: username,
-    });
-
-    return user?.contributions?.nodes || [];
   }, {
     maxAge: 60 * 60, // 1 hour
     swr: true,
--- api/v1/mosaic/projects.get.ts
+++ api/v1/mosaic/projects.get.ts
@@ -1,6 +1,8 @@
 export default defineLazyEventHandler(async () => {
   const runtimeConfig = useRuntimeConfig();
   return defineEventHandler(async (event) => {
+    // eslint-disable-next-line no-console
+    console.log("event", event);
     return proxyRequest(event, `${runtimeConfig.worker}/repositories`);
   });
 });
